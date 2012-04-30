96b9e739aaf422bbc6a374a78aec939543a1d5cb
qiubao.tang <qiubao.tang@ab-software.org>
2012-04-30 22:09:06 +0800

    add socket impl

---
+++ dtm-socket-impl/src/main/java/org/ab_software/dtm/impl/socket/DataTransferServiceImpl.java
@@ -0,0 +1,15 @@
+package org.ab_software.dtm.impl.socket;
+
+import org.ab_software.dtm.DataTransferService;
+
+public class DataTransferServiceImpl implements DataTransferService {
+
+	public void sendDataToRemote(byte[] data, String remoteHost, int port) {
+
+	}
+
+	public byte[] getDataFromRemote(String remoteHost, int port) {
+		return null;
+	}
+
+}
